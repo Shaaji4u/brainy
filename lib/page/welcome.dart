039c0c8da598f015fc7ef5f6946af4e50be2a6b5
Shaaji Alfred <shaajialfred@gmail.com>
2020-06-09 18:28:44 +0100

    IQ Screen Added

--- lib/page/welcome.dart
+++ lib/page/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:brainy/page/iq.dart';
 import 'package:brainy/page/result.dart';
 import 'package:flutter/material.dart';
 
@@ -31,14 +32,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   void registerUser(){
     validateUserInput();
     if(validatorErrorText == null && usernameTextController.text.isNotEmpty){
-      
-      Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>ResultPage(
-        correctScore: 70.0,
-        wrongScore: 30.0,
-        totalScore: 100,
-        result: "Output Will be displayed here\nThanks",
-      )));
-      
+      Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>IQTestScreen()));
       print(".........Register User...........");
       
             
@@ -133,7 +127,3 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     );
   }
 }
-
-
-
-
